00c1ef168ccbf47012b3025d9fb528ef79d7ebaa
Thomas @ BeeDesk <thomasleaf@gmail.com>
2016-02-15 11:27:10 -0800

    ✔ Added ModelAdmin for Role and m-to-m models.

--- trusts/admin.py
+++ trusts/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from trusts.models import Trust
+from trusts.models import Trust, Role, RolePermission, TrustUserPermission
 
 admin.site.register(Trust, admin.ModelAdmin)
+admin.site.register(Role, admin.ModelAdmin)
+admin.site.register(RolePermission, admin.ModelAdmin)
+admin.site.register(TrustUserPermission, admin.ModelAdmin)
+
